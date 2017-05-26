ebb69a570fbfcce5c954218eaaec80fd481ccbe3
kiyohito.itoh <ito.kiyohito@tis.co.jp>
2017-05-26 12:16:19 +0900

    Add redmine backlogs plugin

---
+++ 3.3/plugins/redmine-backlogs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+docker-compose exec redmine bash -c "apt-get update -y \
+&& cd plugins \
+&& git clone https://github.com/backlogs/redmine_backlogs.git \
+&& cd redmine_backlogs \
+&& git branch -r \
+&& git checkout origin/feature/redmine3 \
+&& sed -i -e 's/gem \"nokogiri\"/#gem \"nokogiri\"/' Gemfile \
+&& sed -i -e 's/gem \"capybara\"/#gem \"capybara\"/' Gemfile \
+&& sed -i -e 's/gem \"thin\"/#gem \"thin\"/' Gemfile \
+&& cd /usr/src/redmine \
+&& bundle install --without development test \
+&& RAILS_ENV=production \
+&& export RAILS_ENV \
+&& bundle exec rake db:migrate \
+&& bundle exec rake tmp:cache:clear \
+&& bundle exec rake tmp:sessions:clear \
+&& bundle exec rake redmine:load_default_data REDMINE_LANG=en \
+&& bundle exec rake redmine:backlogs:install story_trackers=Bug task_tracker=Bug"
+
+docker-compose restart redmine
